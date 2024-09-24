d4d223ae0091767a302665e235abd902eadd2bdf
racool <ridwan.rede02@gmail.com>
2024-09-24 00:28:50 +0000

    update script

--- ansible-setup.sh
+++ ansible-setup.sh
@@ -1,4 +1,3 @@
+sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt update
-sudo apt install software-properties-common
-sudo add-apt-repository --yes --update pa:ansible/ansible
 sudo apt install ansible -y
